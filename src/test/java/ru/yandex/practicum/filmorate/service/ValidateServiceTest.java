8b8c41a4ed75b33e22554cf4ccd66c05efae3025
Tanya Vyatkina <tanya.vyatkina.nn@yandex.ru>
2023-08-31 15:27:06 +0300

    fix : лишний импорт

--- src/test/java/ru/yandex/practicum/filmorate/service/ValidateServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/ValidateServiceTest.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
