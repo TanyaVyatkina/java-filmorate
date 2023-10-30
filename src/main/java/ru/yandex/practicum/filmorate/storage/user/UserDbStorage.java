5f6d82f393091e72d31c33272e4b90ff60f537b9
Anton P <anton.neongogo@yandex.ru>
2023-10-30 16:37:41 +0300

    Исправил замечания, касаемые называний методов и лишней проверки на наличие фильма/пользователя перед их удалением Удалил неиспользуемый импорт

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.sql.ResultSet;
